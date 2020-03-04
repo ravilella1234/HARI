ad7ab7e51e24d5984bb87add259a7580949cd678
maha <maha@gmail.com>
2020-03-04 11:40:22 +0530

    Selenium

---
+++ MDBatch/src/com/launch/TC_002.java
@@ -0,0 +1,14 @@
+package com.launch;
+
+public class TC_002 extends BaseTest
+{
+
+	public static void main(String[] args) 
+	{
+		browserLaunch("firefox");
+		
+		navigateUrl("https://www.bestbuy.com");
+
+	}
+
+}
